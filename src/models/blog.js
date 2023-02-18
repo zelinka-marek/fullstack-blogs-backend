82c5a967e35ac0584c6c80421ad0d2cd19853d5b
Marek Zelinka <mzelinka17@gmail.com>
2023-02-18 16:42:58 +0100

    add basic validation

--- src/models/blog.js
+++ src/models/blog.js
@@ -1,10 +1,19 @@
 import mongoose from "mongoose";
 
 const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number,
+  title: { type: String, trim: true, required: [true, "Title is required"] },
+  author: { type: String, trim: true, required: [true, "Author is required"] },
+  url: {
+    type: String,
+    trim: true,
+    required: [true, "Url is required"],
+    validate: {
+      validator: (value) =>
+        value.includes("http://") || value.includes("https://"),
+      message: (props) => `${props.value} is not a valid url!`,
+    },
+  },
+  likes: { type: Number, default: 0 },
 });
 
 export const Blog = mongoose.model("Blog", blogSchema);
