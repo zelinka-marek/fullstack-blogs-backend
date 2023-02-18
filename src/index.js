e9ff7c2cfea70438b16b9d287bfc518677879016
Marek Zelinka <mzelinka17@gmail.com>
2023-02-18 16:31:56 +0100

    add middleware

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ import mongoose from "mongoose";
 import { blogsRouter } from "./routes/blogs.js";
 import { MONGODB_URI, PORT } from "./utils/config.js";
 import { logError, logInfo } from "./utils/logger.js";
+import {
+  errorHandler,
+  requestLogger,
+  unknownEndpoint,
+} from "./utils/middleware.js";
 
 logInfo("connecting to db");
 mongoose
@@ -14,5 +19,8 @@ mongoose
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(requestLogger);
 app.use("/api/blogs", blogsRouter);
+app.use(errorHandler);
+app.use(unknownEndpoint);
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
