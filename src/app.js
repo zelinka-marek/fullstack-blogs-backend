e9aa79a164ca61413676c254a7655ebca51cd51e
Marek Zelinka <mzelinka17@gmail.com>
2023-02-18 16:35:08 +0100

    move express app

---
+++ src/app.js
@@ -0,0 +1,35 @@
+import cors from "cors";
+import express from "express";
+import mongoose from "mongoose";
+import { blogsRouter } from "./routes/blogs.js";
+import { MONGODB_URI } from "./utils/config.js";
+import { logError, logInfo } from "./utils/logger.js";
+import {
+  errorHandler,
+  requestLogger,
+  unknownEndpoint,
+} from "./utils/middleware.js";
+
+mongoose.set("strictQuery", false);
+mongoose.set("toJSON", {
+  transform: (_document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+logInfo("connecting to db", MONGODB_URI);
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => logInfo("connected to db"))
+  .catch((error) => logError("error connecting to db:", error.message));
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(requestLogger);
+app.use("/api/blogs", blogsRouter);
+app.use(unknownEndpoint);
+app.use(errorHandler);
+app.listen(PORT, () => logInfo(`Server running on port ${PORT}`));
--- src/index.js
+++ src/index.js
@@ -1,26 +1,5 @@
-import cors from "cors";
-import express from "express";
-import mongoose from "mongoose";
-import { blogsRouter } from "./routes/blogs.js";
-import { MONGODB_URI, PORT } from "./utils/config.js";
-import { logError, logInfo } from "./utils/logger.js";
-import {
-  errorHandler,
-  requestLogger,
-  unknownEndpoint,
-} from "./utils/middleware.js";
+import { app } from "./app.js";
+import { PORT } from "./utils/config.js";
+import { logInfo } from "./utils/logger.js";
 
-logInfo("connecting to db");
-mongoose
-  .connect(MONGODB_URI)
-  .then(() => logInfo("connected to db"))
-  .catch((error) => logError("failed connecting to db", error.message));
-
-const app = express();
-app.use(cors());
-app.use(express.json());
-app.use(requestLogger);
-app.use("/api/blogs", blogsRouter);
-app.use(errorHandler);
-app.use(unknownEndpoint);
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => logInfo(`Server running on port ${PORT}`));
