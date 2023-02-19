d3fb101e1cff20b633768fe4bd9a41a9d5c568dc
Marek Zelinka <mzelinka17@gmail.com>
2023-02-19 12:32:49 +0100

    solve ex4.4 - test totalList util

--- src/utils/list.js
+++ src/utils/list.js
@@ -1,4 +1,8 @@
 // eslint-disable-next-line no-unused-vars
-export function dummy(_array) {
+export function dummy(_blogs) {
   return 1;
+}
+
+export function totalLikes(blogs) {
+  return blogs.reduce((likes, blog) => likes + blog.likes, 0);
 }
