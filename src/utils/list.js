33bbbe18a3d91755f61b552399d571e37b2d6eff
Marek Zelinka <mzelinka17@gmail.com>
2023-02-19 12:39:27 +0100

    solve ex4.5 - favoriteBlog

--- src/utils/list.js
+++ src/utils/list.js
@@ -6,3 +6,18 @@ export function dummy(_blogs) {
 export function totalLikes(blogs) {
   return blogs.reduce((likes, blog) => likes + blog.likes, 0);
 }
+
+export function favoriteBlog(blogs) {
+  if (!blogs.length) {
+    return null;
+  }
+
+  const maxLikes = Math.max(...blogs.map((blog) => blog.likes));
+  const favoriteBlog = blogs.find((blog) => blog.likes === maxLikes);
+
+  return {
+    title: favoriteBlog.title,
+    author: favoriteBlog.author,
+    likes: maxLikes,
+  };
+}
