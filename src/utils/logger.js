2380500392ac2031bb534ae60e1b225a5888eb6b
Marek Zelinka <mzelinka17@gmail.com>
2023-02-20 08:29:17 +0100

    do not log during tests

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,11 @@
 export function logInfo(...params) {
-  console.log(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.log(...params);
+  }
 }
 
 export function logError(...params) {
-  console.log(...params);
+  if (process.env.NODE_ENV !== "test") {
+    console.error(...params);
+  }
 }
