a2cbd477c1ff35f744c25bc8e83b22b084536f90
Marek Zelinka <mzelinka17@gmail.com>
2023-02-20 08:51:43 +0100

    solve ex4.10 - add blog

--- src/routes/blogs.js
+++ src/routes/blogs.js
@@ -8,8 +8,8 @@ blogsRouter.get("/", async (_request, response) => {
   response.json(blogs);
 });
 
-blogsRouter.post("/", (request, response) => {
-  new Blog(request.body)
-    .save()
-    .then((result) => response.status(201).json(result));
+blogsRouter.post("/", async (request, response) => {
+  const blog = new Blog(request.body);
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
--- tests/blog-api.test.js
+++ tests/blog-api.test.js
@@ -2,7 +2,7 @@ import supertest from "supertest";
 import { beforeEach, expect, test } from "vitest";
 import { app } from "../src/app.js";
 import { Blog } from "../src/models/blog.js";
-import { initialBlogs } from "./blog-helpers.js";
+import { getBlogsFromDatabase, initialBlogs } from "./blog-helpers.js";
 
 const api = supertest(app);
 
@@ -28,4 +28,20 @@ test("blogs have a unique identifier property named id", async () => {
   for (const id of ids) {
     expect(id).toBeDefined();
   }
+});
+
+test("a valid blog can be added", async () => {
+  const validBlog = {
+    title: "React patterns",
+    author: "Michael Chan",
+    url: "https://reactpatterns.com/",
+  };
+  const response = await api.post("/api/blogs").send(validBlog);
+  expect(response.status).toBe(201);
+  expect(response.header["content-type"]).toMatch(/application\/json/);
+
+  const blogsAtEnd = await getBlogsFromDatabase();
+  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1);
+  const authors = blogsAtEnd.map((blog) => blog.author);
+  expect(authors).toContainEqual(validBlog.author);
 });
